80019bf4bf04c9c12d3872f60a2d93a01abe73d9
Nitchamon <Aonoony@gmail.com>
2021-12-28 19:35:35 +0700

    "lab14_2.cpp completed"

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,26 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[N][M]){
+    for(int i = 0; i<72; i++) cout << '-';
+    cout << endl;
+    for(int m = 0; m<30; m++){
+        cout << '|';
+        for(int n = 0; n<70; n++){
+            if(image[m][n] == 0) cout << ' ';
+            else if(image[m][n] == 1) cout << '*';
+        }
+        cout << '|' << endl;
+    }
+    for(int j = 0; j<72; j++) cout << '-';
+    cout << endl;
+}
+
+void updateImage(bool image[N][M], int x, int y, int z){
+    for(int k = 0; k<30; k++){
+        for(int l = 0; l<70; l++){
+            if(sqrt(pow(k-y, 2)+pow(l-z, 2)) <= x-1) image[k][l] = 1;
+        }
+    }
+}
